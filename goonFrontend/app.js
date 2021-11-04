a8396c6e8e976b29de0b032cdf37ef1cc5df069d
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2021-11-04 08:26:12 -0400

    edit Procfile to reflect folder rearrangement

--- goonFrontend/app.js
+++ goonFrontend/app.js
@@ -7,13 +7,3 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(process.env.PORT || 3000)
-
-const userAction = async () => {
-  const response = await fetch('https://goon-backend.herokuapp.com/');
-  const myJson = await response.json();
-  console.log(myJson)
-}
-
-function yerr() {
-  console.log("yerr")
-}
